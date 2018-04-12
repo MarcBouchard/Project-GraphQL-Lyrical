095fb789e1351a86818533574c5c8c7c0ce80c81
Marc Bouchard <mab.bouchard@gmail.com>
2018-04-12 17:14:17 -0400

    feat(queries): create getSong query

--- client/queries/queries.js
+++ client/queries/queries.js
@@ -1,6 +1,7 @@
 //--------------------------------------- Global Imports --
 import gql from 'graphql-tag'
 
+
 //---------------------------------------------- Queries --
 export const fetchSongs = gql` {
   songs {
@@ -8,4 +9,13 @@ export const fetchSongs = gql` {
     title
   }
 }
+`
+
+export const getSong = gql`
+  query GetSong($id: ID!) {
+    song(id: $id) {
+      id
+      title
+    }
+  }
 `
